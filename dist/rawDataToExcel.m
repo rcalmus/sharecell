36fe6d18345e15f589a06520414dff19389f1a32
Ryan Calmus <55794415+rcalmus@users.noreply.github.com>
2024-03-13 05:47:56 -0500

    Update rawDataToExcel.m

--- dist/rawDataToExcel.m
+++ dist/rawDataToExcel.m
@@ -3,8 +3,10 @@
 % section will result in a separate Excel file, and each figure will result in its own Excel worksheet
 % (tab). Each figure may contain multiple tables, e.g. for multiple panels or to describe multiple
 % features of a single plot. Each table will be written to a separate table within the given worksheet,
-% titled according to the name of the field, with optional row and column labels. rawData should be
-% organized as follows (square brackets denote optional fields):
+% titled according to the name of the field, with optional row and column labels. Row and column headings
+% will be drawn from the MATLAB table containing the data to export. Additional labels spanning the entire
+% set of columns and rows can optionally be specified. rawData should be organized as follows (square
+% brackets denote optional fields):
 %    rawData.paperSection.figureName.tables(1:n).table = <MATLAB table of data>
 %    [rawData.paperSection.figureName.tables(1:n).colLabelHorizontal = <char label for columns of table>]
 %    [rawData.paperSection.figureName.tables(1:n).rowLabelVertical = <char label for rows of table>]
